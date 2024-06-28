c142d2b451960e98ab1f1c5762209912024a3b6a
ViniBelo <vinialvesbelo@outlook.com>
2024-06-28 19:26:25 -0300

    Sign In working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,8 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to signup_path, notice: "Successfully created account!"
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully created account!"
     else
       render :new, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "registrations#new"
+
   get "signup", to: "registrations#new"
   post "signup", to: "registrations#create"
+
+  get "signin", to: "sessions#new"
+  post "signin", to: "sessions#create"
 end
